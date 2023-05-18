1346676ac026421c7a71f97608711bc2afb114d9
Juancho432 <juanmanueljaramillojaramillo4@gmail.com>
2023-05-17 19:05:33 -0500

    Update 1.0

--- ShieldedPassword.cpp
+++ ShieldedPassword.cpp
@@ -7,7 +7,14 @@
 
 using namespace std;
 
-    	   
+
+int callback(void* NotUsed, int argc, char** argv, char** azColName){
+    for (int i = 0; i < argc; i++){
+        cout<<azColName[i] <<": " << (argv[i] ? argv[i] : "NULL") << endl;
+    }
+    return 0;
+}
+
 string cifrar(const string& frase) {
     string cifrada = "";
     int desplazamiento = 23;
@@ -48,23 +55,67 @@ string descifrar(const string& fraseCifrada) {
     return descifrada;
 }
 
+int newPassword(sqlite3* db, int rc){
+    string passRAW, user, site;
+	cout<<"Ingrese el Usuario: "; 
+    cin>>user;
+	cout<<"\nIngrese el Sitio: "; 
+    cin>>site;
+    cout<<"\nIngrese la Contraseña: "; 
+    cin>>passRAW;
+                
+    string passCifrada = cifrar(passRAW);
+    string setUser = "UPDATE Passwords SET user = '" + user + "';";
+    string setSite = "UPDATE Passwords SET site = '" + site + "';";
+    string setPass = "UPDATE Passwords SET pass = '" + passCifrada + "';";
+
+    cout<<"Contraseña guardada!!"<<endl;
+    rc = sqlite3_exec(db, setUser.c_str(), 0, 0, 0);
+    return 0;
+}
+
+int viewPasswords(sqlite3* db, int rc){
+    const char *sql = "SELECT site FROM Passwords;";
+    sqlite3_stmt *stmt;
+    rc = sqlite3_prepare_v2(db, sql, -1, &stmt, nullptr);
+    
+    
+    while (sqlite3_step(stmt)== SQLITE_ROW){
+        const char *valor = reinterpret_cast<const char *>(sqlite3_column_text(stmt, 0));
+        cout<< valor <<endl;
+    }
+
+    string option;
+    cout<<"Ingrese el sitio de la contraseña: ";
+    cin>>option;
+
+    string resultSQL = "SELECT user, pass FROM Passwords WHERE site = '" + option + "';";
+    char* zErrMsg;
+    rc = sqlite3_exec(db, resultSQL.c_str(), callback, 0, &zErrMsg);
+
+
+    return 0;
+
+}
 
 int main(){
     sqlite3* db;
-    
-    int rc = sqlite3_open("Passwords.db", &db);
+    int rc;
+    ifstream database;
+    database.open("Passwords.db");
 
-    if (rc != SQLITE_OK)
-    {
+    if (database){
+        rc = sqlite3_open("Passwords.db", &db);
+    }else{
+        cout<<"Creando Base de Datos..."<<endl;
         ofstream file;
         file.open("Passwords.db");
         file.close();
-        string DBpassRAW;
-        cout<<"Ingrese una contraseña para la base de datos: ";
-        cin>>DBpassRAW;
         
-        const char* sql_1 = "CREATE TABLE Passwords (_db_password TEXT NOT NULL, user TEXT NOT NULL DEFAULT [No Set], site TEXT NOT NULL DEFAULT [No Set], pass TEXT NOT NULL DEFAULT [No Set])";
-
+        rc = sqlite3_open("Passwords.db", &db);
+        cout<<"Configurando Base de Datos...."<<endl;
+        const char* sql_1 = "CREATE TABLE Passwords (user TEXT NOT NULL, site TEXT NOT NULL, pass TEXT NOT NULL)";
+        
 		char* errMsg;
         rc = sqlite3_exec(db, sql_1, 0, 0, &errMsg);
         if (rc != SQLITE_OK) {
@@ -73,63 +124,45 @@ int main(){
             sqlite3_free(errMsg);
             sqlite3_close(db);
             return 1;
-        }							
+        }
     }
+    
+    init:
 
-
-		while (bool login = false){
-			system("cls");
-			string loginPassRAW, _db_password;
-			cout<<"Ingrese la contraseña de la base de datos: ";
-			cin>>loginPassRAW;
-			//Consultar _db_password
-			if (loginPassRAW == _db_password){
-				login = true;			
-			}
-		}
-
-        system("cls");
+        cout<<"-----------------------------------------------------------------------"<<endl;
         cout<<"Seleccione una opcion del menu: \n"
                 "   [1] Ingresar nueva contrasena \n"
                 "   [2] Ver contrasenas almacenadas \n"
                 "   [3] Salir \n";
+
+               
         int option;
+        numErr: 
         cin>>option;
+        
 
-
-    while (true) 
-    {
         if(option == 1 or option == 2 or option == 3)
         {
             switch (option)
         	{
             case 1: 
-                char pass[100], user[100], site[100];
-	            cout<<"Ingrese el Usuario: "; cin.getline(user, 100);
-	            cout<<"Ingrese el Sitio: "; cin.getline(site, 100);
-                cout<<"Ingrese la Contraseña: "; cin.getline(pass,100);
-                int i, j, length;
-                length = strlen(pass);
-            break;
+                newPassword(db, rc);
+                goto init;
+                break;
             case 2: 
-            	int option;
-                
-                //const char* siteRequest = sqlite3.exec();
-                
-                // TODO: Get Sites
-                cout<<"Seleccione un Sitio para ver la contraseña: ";
-                cin>>option; 
+                viewPasswords(db, rc);
+                goto init;            	
+                break;
+            case 3: 
+                sqlite3_close(db);
+                cout<<"[X] Saliendo...";
                 return 0;
-            break;
-            case 3: return 0;
-            break;
+                break;
         	}
             
         }else{
-            cout<<"[X] Ingrese un numero valido!!";
+            cout<<"[X] Ingrese un numero valido!! \n";
+            goto numErr;
         }   
-    }
-
-    sqlite3_close(db);
-    return 0;
+    
 }
